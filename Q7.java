d21bcad7ff0aa206700be4e84b9bbb3f39178cc7
sayantanisaha376 <sayantanisaha376@gmail.com>
2025-01-07 22:35:56 +0530

    Update Q7.java

--- Q7.java
+++ Q7.java
@@ -1,32 +1,44 @@
-// Author Sayantani
 
-class Q7 {
-      static int getLength(String str) {
-        return str.length();                        //Method for getting String length
-    }
-
+public class GreetUser {
     public static void main(String[] args) {
-        processUsernames(args);                     //Main method
+        if (args.length < 3) {
+            System.out.println("Please provide at least three usernames as command-line arguments.");
+            return;
+        }
+
+        Usernames(args);
     }
 
-    static void processUsernames(String[] args) {
-        if (args.length < 3) {
-            return;
-        }                                             //Switch case Logic
-        for (int i = 0; i < args.length; i++) {
-            int length = getLength(args[i]);
-            switch (length) {
-                case 4:
-                case 5:
-                case 6:
-                case 7:
-                case 8:
-                    System.out.println("Hello " + args[i] + ", Username length is: " + length);
-                    break;
-                default:
-                    System.out.println("Sorry, the username is too short");
-                    break;
-            }
+    // Method to calculate the length of a string
+    static int getLength(String str) {
+        return str.length();
+    }
+
+    // Method to process usernames and greet the user if length > 3
+    static void Usernames(String[] args) {
+        for (String username : args) {
+            int length = getLength(username);
+            greetUser(username, length);
+        }
+    }
+
+    // Method to greet the user based on username length using switch case
+    static void greetUser(String username, int length) {
+        switch (length) {
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+                System.out.println("Hello " + username + ", Username length is: " + length);
+                break;
+            default:
+               else {
+                    System.out.println("Enter valid username..........");
+                }
+                break;
         }
     }
 }

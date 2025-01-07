c245b7c391e3b10d96f421db2ffa1795c4e74510
sayantanisaha376 <sayantanisaha376@gmail.com>
2025-01-07 23:24:59 +0530

    Add files via upload

---
+++ Q9.java
@@ -0,0 +1,34 @@
+public class Q9 {
+    public static void main(String[] args) {
+        handleCommandLineInput(args);
+    }
+	                                    // Method to handle command line input
+
+                               
+    static void handleCommandLineInput(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Please provide a number as a command-line argument.");
+            return;
+        }
+		                                 // Parse the input number 
+
+        
+        int num = Integer.parseInt(args[0]);
+                                       // Call to calculate and print factorial
+        printFactorial(num);
+    }
+
+                                      // Method to print the factorial of a number
+    static void printFactorial(int num) {
+        System.out.println("Factorial of " + num + " is: " + factorial(num));
+    }
+
+                                    // Method to calculate the factorial of a number
+    static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
+
